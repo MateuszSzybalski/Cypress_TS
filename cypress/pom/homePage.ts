06760699d7c32af7795ad28181df121830e17b82
Mateusz Szybalski <mat.szybalski@gmail.com>
2024-09-12 12:08:50 +0200

    first commit

---
+++ cypress/pom/homePage.ts
@@ -0,0 +1,26 @@
+
+import { CommonMethods } from "./commonMethods"
+
+export class HomePage extends CommonMethods{
+
+    //Selectors
+    
+    basketcounterNumber: string = '.counter-number'
+    loggedIn: string = '.logged-in'
+  
+
+    //Methods
+
+    openHomePage() {
+        cy.visit('https://magento.softwaretestingboard.com/')
+        cy.get(this.basketcounterNumber).should('not.be.visible')
+    }
+
+    selectHeaderLink(headerLinkText: string) {
+        cy.get('.header.links').contains(headerLinkText).click()
+
+    }
+  
+  }
+  
+  export const homePage = new HomePage();
