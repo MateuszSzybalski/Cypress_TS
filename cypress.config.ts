43205d8daff2dafad21a5bdbb138770aa4fe7185
Mateusz Szybalski <mat.szybalski@gmail.com>
2024-09-16 01:14:25 +0200

    update

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    supportFile: 'cypress/support/e2e.{js,jsx,ts,tsx}',
+    //supportFile: 'cypress/support/e2e.{js,jsx,ts,tsx}',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     baseUrl: 'https://www.google.pl',
     viewportHeight: 600,
